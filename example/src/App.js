dc33a87395011827e8fc3275bc71934a39c55a51
JavaScript Joe <jjprevite@gmail.com>
2019-07-04 10:14:06 -0700

    Reafactor App.js to functional component

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import TwilioLogo from '@jsjoeio/react-use-the-things'
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <TwilioLogo
-          height='240px'
-          width='520px'
-          customStyles={{ border: '1px solid black' }}
-      />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <TwilioLogo
+        height='240px'
+        width='520px'
+        customStyles={{ border: '1px solid black' }}
+    />
+    </div>
+  )
 }
+
+export default App
